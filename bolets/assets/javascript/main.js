45985be6761eedd9a1cfe91640851dbce0ca13c8
GLBC <mymailglbc@gmail.com>
2020-06-28 15:09:23 -0400

    Add new Project

---
+++ bolets/assets/javascript/main.js
@@ -0,0 +1,20 @@
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link) {
+  link.addEventListener("click",function(evento){
+    evento.preventDefault();
+    let content = document.querySelector('.content');
+    content.classList.remove("animate__bounce");
+    content.classList.remove("animate__animated");
+    content.classList.add("animate__fadeOutUp");
+    content.classList.add("animate__animated");
+
+    setTimeout(function(){
+      location.href = "/";
+    },1000);
+
+    return false;
+
+  });
+});
